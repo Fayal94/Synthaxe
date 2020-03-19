0f841985dfe9a753f70afd7f9faefe347b031811
fayal94 <mfayal@hotmail.fr>
2020-03-19 17:19:49 +0100

    Arabre romain sans la logique

---
+++ exo_4.py
@@ -0,0 +1,8 @@
+def arabeRomain(a):
+     if romain ==1:
+            if romain ==2:
+                if romain ==12:
+                  return romain""
+print(arabeRomain(1))
+print(arabeRomain(12))
+       
