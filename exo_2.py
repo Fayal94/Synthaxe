41bd755186a67f3a894378183cc0e26976580107
fayal94 <mfayal@hotmail.fr>
2020-03-19 17:27:05 +0100

    addition de deux entiers

---
+++ exo_2.py
@@ -0,0 +1,9 @@
+def addition(a,b):
+     """int= a,b"""
+         input(a)
+         input(b)
+         c= int(a)
+         d= int(b)
+         print(a+ b)
+
+addition(int (input("Taper le premier nombre: " )),int (input("Puis le deuxi    eme: ")))
