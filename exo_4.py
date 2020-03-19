38f82a18519e32ad1a0e3d70b7d577493b50e172
fayal94 <mfayal@hotmail.fr>
2020-03-19 17:44:38 +0100

    voici la premiere methode que j'avais choisi

--- exo_4.py
+++ exo_4.py
@@ -1,8 +1,12 @@
 def arabeRomain(a):
-     if romain ==1:
+    if a == 1:
+        return "I"
+    if a == 12:
+        return "XII"
+     """if romain ==1:
             if romain ==2:
                 if romain ==12:
-                  return romain""
+                  return romain"""
 print(arabeRomain(1))
 print(arabeRomain(12))
        
