812ad6d76569f3fdaf066d8e979e02cb450018c9
fayal94 <mfayal@hotmail.fr>
2020-03-19 17:30:36 +0100

    Fonctions pour ce presenter

---
+++ exo_3.py
@@ -0,0 +1,13 @@
+ def presentation(a,b,c):
+     a = 25
+     b = "Fayal"
+     c = str(H) 
+     H = "je suis un homme"
+     """F = je suis une femme"""
+     
+     d = str(a)
+     """e = str(H)""" 
+     """g = str(F)"""
+     print("Bonjour", c , "Je m appel " + b, "et j ai " + d ,"ans")
+
+ presentation("", "","");
